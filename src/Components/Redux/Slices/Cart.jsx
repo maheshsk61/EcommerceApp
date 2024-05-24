0834607bc2a3ba84e24a4d8df79435533009e2d0
maheshsk61 <maheshsk61@gmail.com>
2024-05-24 17:55:52 +0530

    'initialCommit'

---
+++ src/Components/Redux/Slices/Cart.jsx
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit'
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        cart: []
+    },
+    reducers: {
+        addProduct: (state, action) => {
+            state.cart.push(action.payload)
+        },
+        removeProduct: (state, action) => {
+            state.cart = state.cart.filter((product) => {
+                const productToRemove = action.payload
+                return product.m_id !== productToRemove && product.l_id !== productToRemove && product.t_id !== productToRemove && product.w_id !== productToRemove && product.b_id !== productToRemove && product.p_id !== productToRemove
+            })
+        }
+    }
+})
+export const { addProduct, removeProduct } = cartSlice.actions
+export default cartSlice.reducer
